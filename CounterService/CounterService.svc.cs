928ce6eca4ace94020bd7b56a06a6ceba037241a
FF <f+git@fida.biz>
2016-07-08 00:46:15 +0200

    wcf prototype

---
+++ CounterService/CounterService.svc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using CounterService.Models;
+using CounterService.Persistence;
+
+namespace CounterService
+{
+    public class CounterService : ICounterService, ICounter
+    {
+        public tCounter Get(Guid guid) => AppSettings.Persistence.Get(guid);
+
+        public tCounter Increment(Guid guid, long @by = 1) => AppSettings.Persistence.Increment(guid, @by);
+
+        public tCounter Set(Guid guid, long value = 0) => AppSettings.Persistence.Set(guid, value);
+
+        public Guid NewGuid() => Guid.NewGuid();
+    }
+}
---
+++ CounterService/ICounterService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel;
+using System.Text;
+using CounterService.Models;
+using CounterService.Persistence;
+
+namespace CounterService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICounterService" in both code and config file together.
+    [ServiceContract]
+    public interface ICounterService
+    {
+        [OperationContract] Guid NewGuid();
+        [OperationContract] tCounter Get(Guid guid);
+        [OperationContract] tCounter Increment(Guid guid, long by = 1);
+        [OperationContract] tCounter Set(Guid guid, long value = 0);
+    }
+}

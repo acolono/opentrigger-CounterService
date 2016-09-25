c729f698e545898165b73d173855b66beb54b1af
FF <f+git@fida.biz>
2016-09-25 22:33:10 +0200

    cleanup references

--- CounterService/ICounterService.cs
+++ CounterService/ICounterService.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.Serialization;
 using System.Security.Cryptography.X509Certificates;
 using System.ServiceModel;

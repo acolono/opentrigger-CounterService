374fed92ae7ce4e255a7e3f6ecb3eefad1f7d83c
FF <f+git@fida.biz>
2016-10-08 04:45:28 +0200

    more detailed error messages for the guid problem

--- src/dncCnt/GuidExtensions.cs
+++ src/dncCnt/GuidExtensions.cs
@@ -5,13 +5,14 @@ namespace dncCnt
 {
     public static class GuidExtensions
     {
-        private static readonly Random _r = new Random();
+        private static readonly Random R = new Random();
+        private static readonly Guid EmptyGuid = new Guid("00000000-0000-0000-0000-000000000000");
         public static Guid NewIfEmpty(this Guid g)
         {
             if (g.Equals(Guid.Empty))
             {
                 var b = new byte[16];
-                _r.NextBytes(b);
+                R.NextBytes(b);
                 g = new Guid(b);
             }
             if (g.Equals(Guid.Empty))
@@ -19,7 +20,9 @@ namespace dncCnt
                 Console.WriteLine("Entropy problem?");
                 g = Guid.NewGuid();
             }
-            if (g.Equals(Guid.Empty)) throw new InvalidDataException($"New Guid's are Empty! {g}");
+            if (g.Equals(Guid.Empty)) throw new InvalidDataException($"New Guid's are empty! {g}");
+            if (g == EmptyGuid) throw new InvalidDataException($"Empty Guid's ane NOT empty! {Guid.Empty}");
+            if (Guid.Empty != EmptyGuid) throw new Exception("1+1=23 -- The word as you know it has ended!");
             return g;
         }
     }
